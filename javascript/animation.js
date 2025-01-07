41aa06c389286be9d1f0d02f0111aed0769a1de0
Emilia Schrøder <emsc0004@stud.kea.dk>
2025-01-07 15:26:30 +0100

    sidste animationer og javascript

--- javascript/animation.js
+++ javascript/animation.js
@@ -24,3 +24,17 @@ function clickReset() {
   document.querySelector("#good_container1").classList.add("pulse");
   document.querySelector("#good_container1").addEventListener("click", clickHandler);
 }
+
+// Tilføjer en event listener til scroll - når brugeren scroller ned vil koden gå i gang
+window.addEventListener("scroll", () => {
+  // Tjekker den vertikale scroll position af siden. Hvis den er større end 0, betyder det at brugeren har scrollet.
+  //   window.scrollY fortæller hvor langt man har scrollet ned ad siden i pixels. Den bliver øget når man scroller ned, og falder når man scroller op
+  if (window.scrollY > 0) {
+    // if tjekker om brugeren har scrollet ned (om værdien er større end 0).
+    // Når værdien er større end 0 bliver classen "scrolled" tilføjet
+    document.querySelector("header").classList.add("scrolled");
+  } else {
+    // Hvis værdien ikke er større end 0 (man er på toppen af siden), bliver classen fjernet
+    document.querySelector("header").classList.remove("scrolled");
+  }
+});
